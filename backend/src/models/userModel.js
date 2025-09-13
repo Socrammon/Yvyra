48e157ccdee2920cc7ad3fe63838a8adfabd3783
Lukaauuu <kaueluan017@gmail.com>
2025-09-12 22:10:49 -0300

    autentificação feita

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,17 @@
+import bcrypt from "bcrypt";
+
+const users = [];
+
+export async function createUser(nome, email, senha) {
+  const hash = await bcrypt.hash(senha, 10);
+  users.push({ nome, email, senha: hash });
+  return { nome, email };
+}
+
+export async function findUserByEmail(email) {
+  return users.find(u => u.email === email);
+}
+
+export async function checkPassword(user, senha) {
+  return bcrypt.compare(senha, user.senha);
+}
